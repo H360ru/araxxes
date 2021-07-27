d1daf6ffe1ba1041be12f4b98463e8c37647853b
Germenzi <kineeetik@gmail.com>
2021-07-27 07:43:27 +0300

    Мелкие изменения

--- modules/UnitSystem/unit.gd
+++ modules/UnitSystem/unit.gd
@@ -3,6 +3,8 @@ extends Node2D
 class_name Unit
 
 export var move_speed := 200.0
+# Годо не дает доступ к энуму твина
+export(int, "TRANS_LINEAR", "TRANS_SINE", "TRANS_QUINT", "TRANS_QUART", "TRANS_QUAD", "TRANS_EXPO", "TRANS_ELASTIC", "TRANS_CUBIC", "TRANS_CIRC", "TRANS_BOUNCE", "TRANS_BACK") var move_pattern
 
 var scene_state:UnitSceneState
 var map_position:Vector2
